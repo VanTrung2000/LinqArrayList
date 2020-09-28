173af198f5948823b09feab127b3c9d5b9ffc9f2
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-28 11:35:51 +0700

    Add project files.

---
+++ LinqArrayList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Resources;
+
+namespace LinqArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList sa = new ArrayList();
+            sa.Add(new Staff(1,"Nam",24));
+            sa.Add(new Staff(2, "Kien", 21));
+            sa.Add(new Staff(3, "Viet", 21));
+            sa.Add(new Staff(4, "Hang", 23));
+            sa.Add(new Staff(5, "Tien", 24));
+
+            var arrList = sa.OfType<Staff>();
+            var arrList1 = from aList in arrList
+                         where (aList.Age < 24)
+                         select aList;
+            foreach (var item in arrList1)
+            {
+                Console.WriteLine("ID :" + item.Id + " Name : " + item.Name + " Age : " + item.Age);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ LinqArrayList/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqArrayList
+{
+    class Staff
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Age { set; get; }
+        public Staff() { }
+        public Staff(int Id,string Name,int Age)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
